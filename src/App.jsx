ed7c82fecc45f55c83f6f193ee1db3c4555abb20
steve-cse <stevebobygeorge@gmail.com>
2024-01-29 14:43:13 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import SideBar from "./components/SideBar/SideBar"
+
+function App() {
+ 
+
+  return (
+    <>
+      <SideBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ChatMessages/ChatMessages.jsx
@@ -0,0 +1,44 @@
+const ChatMessages = [
+    { user: true, text: "Hey, what's up?" },
+    { user: false, text: "Not much, just here to chat. How about you?" },
+    { user: true, text: "I had a great day! How about you?" },
+    {
+        user: false,
+        text: "That's awesome to hear! Mine was good too. Anything exciting happen?",
+    },
+    { user: true, text: "Not really, just the usual. How about you?" },
+    {
+        user: false,
+        text: "Just some work and catching up on hobbies. Anything on your mind?",
+    },
+    {
+        user: true,
+        text: "I'm thinking of trying a new recipe for dinner. Any suggestions?",
+    },
+    {
+        user: false,
+        text: "How about a pasta dish? I can share a simple recipe if you'd like.",
+    },
+    {
+        user: true,
+        text: "Sure, that sounds delicious! Please share the recipe.",
+    },
+    {
+        user: false,
+        text: "Great! Start by boiling some pasta and prepare a tomato-based sauce with garlic and herbs.",
+    },
+    {
+        user: true,
+        text: "Sounds easy enough! I'll give it a try tonight. Thanks!",
+    },
+    {
+        user: false,
+        text: "You're welcome! Let me know how it turns out. Enjoy your cooking!",
+    },
+    {
+        user: true,
+        text: "Sounds easy enough! I'll give it a try tonight. Thanks!",
+    },
+];
+
+export default ChatMessages;
