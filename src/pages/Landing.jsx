90acdd2872cf7a7b57521fe1d421601b6fc6d7af
steve-cse <stevebobygeorge@gmail.com>
2024-01-31 15:55:43 +0530

    Integrated supabase and created landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+export default function Landing() {
+    return (
+        <Grid container component="main" sx={{ height: "100vh" }}>
+            <CssBaseline />
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: "url(https://source.unsplash.com/featured/?quotes)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundColor: (t) =>
+                        t.palette.mode === "light"
+                            ? t.palette.grey[50]
+                            : t.palette.grey[900],
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <img
+                        src="/logo.png"
+                        alt="Your Image"
+                        style={{ width: "180px", marginBottom: "20px" }}
+                    />
+                    <Typography
+                        component="h1"
+                        variant="h3"
+                        sx={{ mb: 4, fontWeight: "bold" }}
+                    >
+                        Welcome to Mello
+                    </Typography>
+                    <Typography variant="h6" sx={{ mb: 4, fontStyle: "oblique" }}>
+                        {" "}
+                        “Your Companion for Mental Health.”
+                    </Typography>
+                    <Button variant="contained" color="primary" sx={{ mb: 2, width: '200px' }}>
+                        Log in
+                    </Button>
+                    <Button variant="contained" color="primary" sx={{ mb: 4 , width: '200px'}}>
+                        Sign up
+                    </Button>
+                    
+                </Box>
+            </Grid>
+        </Grid>
+    );
+}
