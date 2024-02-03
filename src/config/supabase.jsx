821d1268bdab2f68f088eac94035827add0e6fe1
steve-cse <stevebobygeorge@gmail.com>
2024-02-03 12:55:03 +0530

    Update env

--- src/config/supabase.jsx
+++ src/config/supabase.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
